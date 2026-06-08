afc0c6606aec877072f585481afc595104b5b319
Deepanshu Tevathiya <deepanshutev@gmail.com>
2026-06-08 18:13:37 +0530

    Train and ADaIN

--- train.py
+++ train.py
@@ -1,19 +1,21 @@
 import argparse
 import torch
-from pathlib import Path
 from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn as nn
 from utils.utils import *
 from utils.model import *
-import torch.optim as optim
+from pathlib import Path
+from tqdm import tqdm
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--content_dir", type=str, default="E:/PROJECTS/MAJOR/NST_APP/content_img",
+    parser.add_argument("--content_dir", type=str, default="E:/PROJECTS/MAJOR/NST_APP/content_img" ,
                         help="Location of style dir.")
-    parser.add_argument("--style_dir", type=str, default="E:/PROJECTS/MAJOR/NST_APP/style_img",
+    parser.add_argument("--style_dir", type=str, default="E:/PROJECTS/MAJOR/NST_APP/style_img" ,
                         help="Location of style dir.")
-    parser.add_argument("--vgg", type=str, default="E:/PROJECTS/MAJOR/NST_APP/vgg_normalised.pth",  #!!
+    parser.add_argument("--vgg", type=str, default="E:/PROJECTS/MAJOR/NST_APP/vgg_normalised.pth" ,  #!!
                         help="Location of vgg model.")
     parser.add_argument("--experiment", type=str, default="experiment1",
                         help="Name of experiment.")
@@ -29,11 +31,22 @@ def parse_arguments():
     
     parser.add_argument('--batch_size', type=int, default=4,
                         help='Batch size')
-    parser.add_argument('--lr', type=float, default='le-4',
+    parser.add_argument('--lr', type=float, default='1e-4',
                         help='Learning Rate')
     parser.add_argument('--lr_decay', type=float, default='5e-5',
                         help='Learning Rate Decay')
     
+    parser.add_argument('--epochs', type=float, default=1,
+                        help='Epochs')
+    parser.add_argument('--content_weight', type=float, default=1.0,
+                        help='content_weight')
+    
+    parser.add_argument('--style_weight', type=float, default=10.0,
+                        help='style_weight')
+    
+    parser.add_argument('--log_interval', type=float, default=1,
+                        help='Log Interval')
+
     return parser.parse_args()
 
 def main():
@@ -52,8 +65,8 @@ def main():
     content_transforms = get_transforms(args.content_size, args.crop, args.final_size)
     style_transforms = get_transforms(args.style_size, args.crop, args.final_size)
 
-    content_dataset = FolderImageDataset(args.content_dir, content_transforms)
-    style_dataset = FolderImageDataset(args.style_dir, style_transforms)
+    content_dataset = FolderImageDataset(args.content_dir , content_transforms)
+    style_dataset = FolderImageDataset(args.style_dir , style_transforms)
 
 
     content_loader = DataLoader(
@@ -84,6 +97,66 @@ def main():
         lr_lambda = lambda epoch : 1.0 / (1.0 + args.lr_decay * epoch)  # return multiplier for lr
     )
 
+    criterion = nn.MSELoss()
+
+    running_loss=None
+    running_closs=None
+    running_sloss=None
+
+    encoder.eval()
+
+    for epoch in range(args.epochs):
+        progress_bar = tqdm(zip(content_loader, style_loader),  # to track the progress
+                            total = min(len(content_loader), len(style_loader)))
+        
+        running_loss=0
+        running_closs=0
+        running_sloss=0
+        for content_batch, style_batch in progress_bar:
+
+            content_batch = content_batch.to(device)
+            style_batch = style_batch.to(device)
+
+            c_feats = encoder(content_batch)      # content feature
+            s_feats = encoder(style_batch)
+ 
+            t = adaptive_instance_normalization(c_feats[-1], s_feats[-1]) # t = features of c and s
+
+            g = decoder(t)  # o/p img from content img and style img features
+
+            g_feats = encoder(g) # features of op img
+
+            c_loss = criterion(g_feats[-1], t) * args.content_weight
+
+            s_loss = 0
+            for g_f, s_f in zip(g_feats, s_feats):
+                g_mean, g_std = get_mean_std(g_f)
+                s_mean, s_std = get_mean_std(s_f)
+
+                s_loss += criterion(g_mean, s_mean) + criterion(g_std, s_std)
+
+            s_loss = s_loss * args.style_weight
+
+            loss = c_loss + s_loss  #loss
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            progress_bar.set_description(f'Loss:{loss.item():4f}, C_Loss:{c_loss.item():4f}, S_Loss:{c_loss.item():4f}')
+
+            running_loss += loss.item() 
+            running_closs += c_loss.item()
+            running_sloss += s_loss.item()
+
+        scheduler.step()
+
+        running_loss /= len(content_loader)
+        running_closs /= len(content_loader)
+        running_sloss /= len(content_loader)
+                
+        if (epoch+1) % args.log_interval == 0:
+            tqdm.write(f'Iter: {epoch+1}  | Loss: {running_loss:4f} | C_loss: {running_closs:4f} | S_loss: {running_sloss:4f}')
 
 
 
--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 
 class FolderImageDataset(Dataset):
     def __init__(self, root, transformation=None):
-        self.root = root,
+        self.root = root
         self.transformation = transformation
         self.files = list(os.listdir(root))
         self.files = [p for p in self.files if p.endswith((".png",".jpg",".jpeg"))]
@@ -15,7 +15,7 @@ class FolderImageDataset(Dataset):
     
     def __getitem__(self, idx):
         file_path = os.path.join(self.root, self.files[idx])
-        image = Image.open(file_path)
+        image = Image.open(file_path).convert('RGB')
 
         if self.transformation:
             image = self.transformation(image)
@@ -33,3 +33,21 @@ def get_transforms(size, crop, final_size):
 
     transforms_list.append(transforms.ToTensor())
     return transforms.Compose(transforms_list)
+
+def adaptive_instance_normalization(content_feat, style_feat):
+    size = content_feat.size()
+    content_mean, content_std = get_mean_std(content_feat)
+    style_mean, style_std = get_mean_std(style_feat)
+    
+    normalized_content_feat = (content_feat - content_mean.expand(size)) / content_std.expand(size)      # (x - mean)/ sigma
+    return normalized_content_feat * style_std.expand(size) + style_mean.expand(size)
+
+def get_mean_std(feat, eps=1e-5):
+    size = feat.size()  # [batch size, n channel, h, w]
+    assert (len(size)==4)
+    batch_size, channels = size[:2]
+                                # 3 dim                         # back to 4 dim
+    feat_mean = feat.view(batch_size, channels, -1).mean(dim=2).view(batch_size, channels, 1, 1)
+    feat_var = feat.view(batch_size, channels, -1).var(dim=2, unbiased=False) + eps
+    feat_std = feat_var.sqrt().view(batch_size, channels, 1, 1)
+    return feat_mean, feat_std
